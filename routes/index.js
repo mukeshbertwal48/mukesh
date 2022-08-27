0476f315e1c13936c1d28672f04f469d7f2e43f7
mukeshbertwal48 <54540391+mukeshbertwal48@users.noreply.github.com>
2022-08-27 16:44:49 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mukesh' });
+  res.render('index', { title: 'Mukesh Bertwal' });
 });
 
 module.exports = router;
