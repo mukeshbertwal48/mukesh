7b18cbfe12ac6989e9785b924c6abeb863ed8d8d
Ubuntu <ubuntu@ip-172-31-4-131.ap-south-1.compute.internal>
2022-08-27 08:55:26 +0000

    latest changes

--- app.js
+++ app.js
@@ -4,21 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var adminupRouter = require('./routes/adminup');
-var addcustRouter = require('./routes/addcust');
-var additemRouter = require('./routes/additem');
-var statuschangeRouter = require('./routes/statuschange');
-var ussigninRouter = require('./routes/ussignin');
-var listRouter = require('./routes/list');
-
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,14 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', usersRouter);
-app.use('/',adminupRouter);
-app.use('/',addcustRouter);
-app.use('/',additemRouter);
-app.use('/',statuschangeRouter);
-app.use('/',ussigninRouter);
-app.use('/',listRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/account', function(req, res, next) {
-  res.render('index',{name: 'Username',pass: 'Password'})
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
-
 
 module.exports = router;
